545532e4a11be5c82c813c3f3fd3b8c8c57d0e0b
Nickolas Piraino <kwnp17c@gmail.com>
2025-09-10 01:15:31 -0400

    Using front-end cookie with roles for middleware authentication;

--- Jwt-Auth/frontend/jwtpsql/src/middleware.ts
+++ Jwt-Auth/frontend/jwtpsql/src/middleware.ts
@@ -8,9 +8,8 @@ export async function middleware(request: NextRequest) {
 
     // const refreshToken = request.cookies.get('refreshToken'); // use NextRequest in middleware to access cookies
     // console.log("refreshToken: " + refreshToken);
-    // const isLoggedIn = request.cookies.get('token'); // use NextRequest in middleware to access cookies
+
     const role = request.cookies.get('role'); // use NextRequest in middleware to access cookies
-    console.log("role: " + role?.toString());
     console.log("role: " + role?.value);
 
     let isLoggedIn = false; // Default to false
@@ -19,51 +18,49 @@ export async function middleware(request: NextRequest) {
     }
     console.log("isLoggedIn: " + isLoggedIn);
 
-    // const roleToken = request.cookies.get('roleToken'); // use NextRequest in middleware to access cookies
-
-    // let isAdmin = false; // Default to false
-    // if (roleToken?.value) {
-    //     const { payload }: { payload: { roles: string[] } } = await jwtVerify(roleToken?.value, new TextEncoder().encode(`${process.env.API_SECRET_KEY}`));
-    //     isAdmin = payload?.roles?.includes('ROLE_ADMIN') || false; // Check if the user has admin roles
-    // }
+    let isAdmin = false; // Default to false
+    if (role?.value.includes('ROLE_ADMIN')) {
+        isAdmin = true; // If roles include 'ROLE_ADMIN', user is admin
+    }
 
     // const isLoginPage = request.nextUrl.pathname.startsWith('/login');
     // const isRegisterPage = request.nextUrl.pathname.startsWith('/register');
     // const isAdminDash = request.nextUrl.pathname.startsWith('/admin-dashboard');
     // // const isUserDash = request.nextUrl.pathname.startsWith('/user-dashboard');
 
-    // const isProtected = ['/dashboard'].some(path =>
-    //     request.nextUrl.pathname.startsWith(path)
-    // );
+    const isProtected = ['/dashboard', '/admin'].some(path =>
+        request.nextUrl.pathname.startsWith(path)
+    );
 
-    // // // If the user is trying to access a protected route without a token
-    // if (isProtected && !isLoggedIn) {
-    //     return NextResponse.redirect(new URL('/login-client', request.url));
-    // }
+    // If the user is trying to access a protected route without a token
+    if (isProtected && !isLoggedIn) {
+        return NextResponse.redirect(new URL('/login-client', request.url));
+    }
 
-    // // // If the user w/ token is trying to access the admin dashboard without admin roles
-    // // if (isLoggedIn && isAdminDash && !isAdmin) {
-    // //     return NextResponse.redirect(new URL('/user-dashboard', request.url));
-    // // }
+    const isAdminDash = request.nextUrl.pathname.startsWith('/admin');
+    // If the user w/ token is trying to access the admin dashboard without admin roles
+    if (isLoggedIn && isAdminDash && !isAdmin) {
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
 
-    // const isLoginPage = request.nextUrl.pathname.startsWith('/login-client');
-    // // // If the user is trying to access the login page while already logged in
-    // if (isLoginPage && isLoggedIn) {
-    //     // if (isAdmin) {
-    //     //     return NextResponse.redirect(new URL('/admin-dashboard', request.url));
-    //     // }
-    //     return NextResponse.redirect(new URL('/dashboard', request.url));
-    //     //Or Redirect to admin dashboard if the user is an admin
-    // }
+    const isLoginPage = request.nextUrl.pathname.startsWith('/login-client');
+    // If the user is trying to access the login page while already logged in
+    if (isLoginPage && isLoggedIn) {
+        if (isAdmin) {        //Or Redirect to admin dashboard if the user has admin roles
+            return NextResponse.redirect(new URL('/admin', request.url));
+        }
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+        //Or Redirect to admin dashboard if the user is an admin
+    }
 
-    // const isRegisterPage = request.nextUrl.pathname.startsWith('/register-client');
-    // // // If the user is trying to access the register page while already logged in
-    // if (isRegisterPage && isLoggedIn) {
-    //     // if (isAdmin) {        //Or Redirect to admin dashboard if the user has admin roles
-    //     //     return NextResponse.redirect(new URL('/admin-dashboard', request.url));
-    //     // }
-    //     return NextResponse.redirect(new URL('/dashboard', request.url));
-    // }
+    const isRegisterPage = request.nextUrl.pathname.startsWith('/register-client');
+    // If the user is trying to access the register page while already logged in
+    if (isRegisterPage && isLoggedIn) {
+        if (isAdmin) {        //Or Redirect to admin dashboard if the user has admin roles
+            return NextResponse.redirect(new URL('/admin', request.url));
+        }
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
 
     return NextResponse.next();
 };
