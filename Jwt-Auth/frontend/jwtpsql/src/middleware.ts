527b8a38a60c053289ba2dcb4ca2b47a1ea33dd4
Nickolas Piraino <kwnp17c@gmail.com>
2025-09-10 01:20:31 -0400

    Using front-end cookie with roles for middleware authentication;

--- Jwt-Auth/frontend/jwtpsql/src/middleware.ts
+++ Jwt-Auth/frontend/jwtpsql/src/middleware.ts
@@ -66,5 +66,5 @@ export async function middleware(request: NextRequest) {
 };
 
 export const config = {
-    matcher: ['/admin', '/login', '/register', '/dashboard', '/login-client'],
+    matcher: ['/admin', '/register-client', '/dashboard', '/login-client'],
 };
